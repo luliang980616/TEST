43832da560e0378cabbab53a5c42f14f656e7772
luliang980616 <128216069+luliang980616@users.noreply.github.com>
2025-03-15 12:32:01 +0800

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace PlaneWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Run();
+        }
+    }
+}
